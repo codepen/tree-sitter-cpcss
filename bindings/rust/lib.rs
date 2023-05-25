18805136bf7ee69d859379eb5244a36432116bdc
Robert Kieffer <robert@broofa.com>
2023-05-25 14:05:22 -0700

    unified parser for extracting dependencies from css dialects

--- bindings/rust/lib.rs
+++ bindings/rust/lib.rs
@@ -1,4 +1,4 @@
-//! This crate provides stylus language support for the [tree-sitter][] parsing library.
+//! This crate provides cssimports language support for the [tree-sitter][] parsing library.
 //!
 //! Typically, you will use the [language][language func] function to add this language to a
 //! tree-sitter [Parser][], and then use the parser to parse some code:
@@ -6,7 +6,7 @@
 //! ```
 //! let code = "";
 //! let mut parser = tree_sitter::Parser::new();
-//! parser.set_language(tree_sitter_stylus::language()).expect("Error loading stylus grammar");
+//! parser.set_language(tree_sitter_cssimports::language()).expect("Error loading cssimports grammar");
 //! let tree = parser.parse(code, None).unwrap();
 //! ```
 //!
@@ -18,14 +18,14 @@
 use tree_sitter::Language;
 
 extern "C" {
-    fn tree_sitter_stylus() -> Language;
+    fn tree_sitter_cssimports() -> Language;
 }
 
 /// Get the tree-sitter [Language][] for this grammar.
 ///
 /// [Language]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Language.html
 pub fn language() -> Language {
-    unsafe { tree_sitter_stylus() }
+    unsafe { tree_sitter_cssimports() }
 }
 
 /// The content of the [`node-types.json`][] file for this grammar.
@@ -47,6 +47,6 @@ mod tests {
         let mut parser = tree_sitter::Parser::new();
         parser
             .set_language(super::language())
-            .expect("Error loading stylus language");
+            .expect("Error loading cssimports language");
     }
 }
