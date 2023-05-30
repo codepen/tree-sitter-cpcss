9944e8241da74a2be0aeb9b491e924f488f61451
Robert Kieffer <robert@broofa.com>
2023-05-30 15:21:02 -0700

    add css corpus, improved comment handling (#3)

--- grammar.js
+++ grammar.js
@@ -1,23 +1,30 @@
 // Grammar for extracting dependencies out of CSS files
+// Grammar for extracting dependencies out of CSS files
 
 module.exports = grammar({
   name: 'cpcss',
+  name: 'cpcss',
 
-  extras: $ => [/[ \t\r\n]/], // "\s" = [ \t\r\n], but we don't want to match newlines
+  extras: $ => [/\s/], // "\s" = [ \t\r\n], but we don't want to match newlines
 
   rules: {
-    doc: $ =>
-      repeat(
-        choice(
-          $.import_statement,
-          $.comment_block,
-          $.comment_line,
-          $.string,
-          $._tbd
-        )
-      ),
+    doc: $ => repeat(choice($.import_statement, $.comment, $.string, $._tbd)),
 
     // Catch-all rule for constructs we don't care about
+    _tbd: $ => prec(-1, repeat1(choice(/./))),
+
+    // Imports
+    import_statement: $ =>
+      seq(
+        choice('@import', '@use', '@forward', '@require'),
+        optional($.less_keywords),
+        $._imported,
+        optional(repeat(seq(',', $._imported)))
+      ),
+
+    _imported: $ => choice($._url, $._quoted_import_reference),
+
+    _url: $ => seq('url', '(', $._quoted_import_reference, ')'),
     _tbd: $ => prec(-1, repeat1(choice(/./))),
 
     // Imports
@@ -43,6 +50,8 @@ module.exports = grammar({
 
     less_keyword: $ => seq(/\w+/, optional(',')),
     less_keywords: $ => seq('(', repeat($.less_keyword), ')'),
+    less_keyword: $ => seq(/\w+/, optional(',')),
+    less_keywords: $ => seq('(', repeat($.less_keyword), ')'),
 
     // Rules for constructs that might contain imports that aren't actually
     // imports (e.g. strings & comments)
@@ -54,11 +63,14 @@ module.exports = grammar({
         )
       ),
 
-    // `/**... */`-style comments.  Regex is from tree-sitter-css. 'Not entirely
-    // sure why it works, but it does.
-    comment_block: $ => token(seq('/*', /[^*]*\*+([^/*][^*]*\*+)*/, '/')),
+    // `/**... */`-style comments.  Regex is from tree-sitter-javascript, which
+    // references this SO answer:
+    // http://stackoverflow.com/questions/13014947/regex-to-match-a-c-style-multiline-comment/36328890#36328890
+    _comment_block: $ => seq('/*', /[^*]*\*+([^/*][^*]*\*+)*/, '/'),
 
     // `// ...`-style comments
-    comment_line: $ => token(seq('//', /.+/)),
+    _comment_line: $ => token(seq('//', /.+/)),
+
+    comment: $ => choice($._comment_block, $._comment_line),
   },
 });
